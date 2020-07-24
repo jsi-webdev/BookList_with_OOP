c4e955c835244dae2ee27b292bc17b666c590626
jsi-webdev <j.skullow@gmail.com>
2020-07-25 04:54:13 +0900

    add localstorage logic

--- appEx6.js
+++ appEx6.js
@@ -8,6 +8,47 @@ class Book {
   }
 }
 
+// Local storage class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function (book) {
+      const ui = new UI();
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
 class UI {
   addBookToList(book) {
     const list = document.getElementById("book-list");
@@ -59,6 +100,9 @@ class UI {
   }
 }
 
+// DOM Load event
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
+
 // Event Listners for add books
 document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get from values
@@ -78,6 +122,8 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
   } else {
     // Add book to list
     ui.addBookToList(book);
+    // Store to LS
+    Store.addBook(book);
     // Show Success adding book
     ui.showAlert("Book added", "success");
     // Clear input field
@@ -92,6 +138,8 @@ document.getElementById("book-list").addEventListener("click", function (e) {
   const ui = new UI();
   // Delete book
   ui.deleteBook(e.target);
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
   // Show message
   ui.showAlert("Book deleted!", "success");
   e.preventDefault();
