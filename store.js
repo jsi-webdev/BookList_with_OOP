c74214ed30771d0ef7fecee8a15f4810063f8204
jsi-webdev <j.skullow@gmail.com>
2020-10-02 01:52:42 +0900

    R8 - Store(LS) Class : delete book to LS

--- store.js
+++ store.js
@@ -28,4 +28,16 @@ class Store {
 
     localStorage.setItem("books", JSON.stringify(books));
   }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
 }
